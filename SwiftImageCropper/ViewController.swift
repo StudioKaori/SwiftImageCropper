236c9d040669fd03170f725b727b051580457edb
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-20 13:54:17 +0100

    Initial Commit

---
+++ SwiftImageCropper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftImageCropper
+//
+//  Created by Kaori Persson on 2023-03-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
