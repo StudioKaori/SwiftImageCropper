c9de5801abf27baca90d226181cd4740f835a410
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-20 14:04:55 +0100

    Add photo selecter

---
+++ README.md
@@ -0,0 +1,7 @@
+#Image cropper by TOCropViewController
+
+Video
+https://www.youtube.com/watch?v=XPcuAg0Xctw
+
+Repo
+https://github.com/TimOliver/TOCropViewController
--- SwiftImageCropper/ViewController.swift
+++ SwiftImageCropper/ViewController.swift
@@ -5,6 +5,15 @@
 //  Created by Kaori Persson on 2023-03-20.
 //
 
+//#Image cropper by TOCropViewController
+//
+//Video
+//https://www.youtube.com/watch?v=XPcuAg0Xctw
+//
+//Repo
+//https://github.com/TimOliver/TOCropViewController
+
+import CropViewController
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +21,19 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+    button.setTitle("Pick Photo", for: .normal)
+    view.addSubview(button)
+    button.center = view.center
+    button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
   }
 
+  @objc func didTapButton() {
+    let picker = UIImagePickerController()
+    picker.sourceType = .photoLibrary
+    present(picker, animated: true)
+  }
 
 }
 
